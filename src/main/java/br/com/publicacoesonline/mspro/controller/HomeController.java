1c8ed8b2ea31d4c1901e8f36be5973b699003db0
djbrunoramon <brmachado_5@hotmail.com>
2021-08-08 15:40:34 -0300

    Teste de Conexao com o Banco de dados

---
+++ src/main/java/br/com/publicacoesonline/mspro/controller/HomeController.java
@@ -0,0 +1,36 @@
+package br.com.publicacoesonline.mspro.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    private DataSource source;
+
+    @GetMapping("/conexao")
+    public String getConnection(){
+        String status = "";
+
+        Connection con = null;
+        try {
+            con = source.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if(con != null){
+            status = "Conexao ok";
+        }else{
+            status = "Erro ao conectar!";
+        }
+
+        return status;
+    }
+}
